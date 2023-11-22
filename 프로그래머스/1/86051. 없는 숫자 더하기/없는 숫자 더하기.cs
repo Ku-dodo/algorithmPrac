3cd838017d7bf16b258dafcb7285f0d76ac5208e
Ku-dodo <105593231+Ku-dodo@users.noreply.github.com>
2023-11-22 11:24:37 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        foreach (int number in numbers)
+        {
+            answer -= number;
+        }
+        return answer;
+    }
+}
