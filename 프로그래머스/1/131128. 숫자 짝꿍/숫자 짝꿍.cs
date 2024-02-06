b1c5dbbb1e7defe0e33cd4a7d87efa10ea2299fe
Ku-dodo <105593231+Ku-dodo@users.noreply.github.com>
2024-02-06 23:30:51 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2.51 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution {
+    public string solution(string X, string Y) {
+        
+        Dictionary<char, int> charY = new Dictionary<char, int>();
+        foreach(var c in Y)
+        {
+            if(!charY.ContainsKey(c))
+            {
+                charY.Add(c, 0);
+            }
+            charY[c]++;
+        }
+        
+        StringBuilder Z = new StringBuilder();
+        foreach(var c in X)
+        {
+            if(charY.ContainsKey(c) && charY[c] > 0)
+            {
+                Z.Append(c);
+                charY[c]--;
+            }
+        }
+        
+        string answer = Z.ToString();
+        
+        if(answer.Length == 0)
+        {
+            return "-1";
+        }
+        else if(answer.Replace("0", "").Length == 0)
+        {
+            return "0";
+        }
+        else
+        {
+            answer = string.Concat(answer.OrderByDescending(ch => ch));
+        }
+        
+        return answer;
+    }
+}
