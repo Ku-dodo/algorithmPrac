c9b6d0f4f80177f3456d40a8fe47d5febd37c16a
Ku-dodo <105593231+Ku-dodo@users.noreply.github.com>
2024-04-10 01:02:53 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.57 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int[] A, int[] B) {
+        Array.Sort(A);
+        Array.Sort(B);
+        int answer = 0;
+        for(int i = 0; i < A.Length; i++)
+        {
+            answer += A[i] * B[B.Length - 1 - i];
+        }
+        return answer;
+    }
+}
