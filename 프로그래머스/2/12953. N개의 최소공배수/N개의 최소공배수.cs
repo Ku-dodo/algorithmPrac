7adb394077a4315fbb1281fa989c17acb27be184
Ku-dodo <105593231+Ku-dodo@users.noreply.github.com>
2025-08-12 20:27:13 +0900

    [level 2] Title: N개의 최소공배수, Time: 2.48 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] arr)
+    {
+
+        Array.Sort(arr);
+        int multiple = 1;
+
+        while (true)
+        {
+            bool bb = true;
+            int a = arr[arr.Length - 1] * multiple;
+            foreach (int i in arr)
+            {
+                if (a % i != 0)
+                {
+                    bb = false;
+                    break;
+                }
+            }
+            if (!bb)
+                multiple++;
+            else
+                break;
+        }
+
+        int answer = arr[arr.Length - 1] * multiple;
+        return answer;
+    }
+}
