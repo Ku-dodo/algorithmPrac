aeda1e19a784c836e9a1f768eaed86c84159b356
Ku-dodo <105593231+Ku-dodo@users.noreply.github.com>
2025-08-13 20:39:35 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.26 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(string my_string) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = my_string.Length - 1; i >= 0; i--){
+            sb.Append(my_string[i]);
+        }
+        return sb.ToString();
+    }
+}
