f49b08233e6d97b55d2fc2b09245803bd094432d
Ku-dodo <105593231+Ku-dodo@users.noreply.github.com>
2025-08-27 22:11:42 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.83 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public int[] solution(int n, string[] words)
+    {
+        HashSet<string> str = new HashSet<string>();
+
+        int count = 1;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (str.Contains(words[i]))
+            {
+                return new int[] { count % n == 0 ? n : count % n, (int)MathF.Ceiling((float)count / n) };
+            }
+            else
+            {
+                str.Add(words[i]);
+            }
+            
+            count++;
+            if (i < words.Length - 1 && words[i][words[i].Length - 1] != words[i + 1][0])
+            {
+                return new int[] { count % n == 0 ? n : count % n, (int)MathF.Ceiling((float)count / n) };
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+}
